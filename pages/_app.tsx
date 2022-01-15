a58f90e0139819ee876e5226d57b6cf919f8852d
Philippe Naegeli <phn@anyweb.ch>
2022-01-15 11:57:59 -0500

    feat: add clerk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
+import { AppProps } from 'next/dist/shared/lib/router/router';
+
+const publicPages: string[] = [];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Get the pathname
+  const { pathname } = useRouter();
+
+  const isPublicPage = publicPages.includes(pathname);
+
+  return (
+    <ClerkProvider>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn  />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 }
 export default MyApp
