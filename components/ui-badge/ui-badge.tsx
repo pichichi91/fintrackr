3f335faec4f614dacc63a20de47c8389664c17f1
Philippe Naegeli <phn@anyweb.ch>
2022-01-20 20:03:41 -0500

    fix: defaut badge styling

--- components/ui-badge/ui-badge.tsx
+++ components/ui-badge/ui-badge.tsx
@@ -9,13 +9,13 @@ const UiBadge: React.FC<BadgeProps> = ({ text, className }) => {
     <>
       {text && (
         <span
-          className={`${className} md:hidden inline-flex items-center uppercase justify-center px-2 py-1 text-xs font-bold leading-none text-white  rounded`}
+          className={`${className} md:hidden inline-flex items-center uppercase justify-center px-2 py-1 text-xs font-bold leading-none   rounded`}
         >
           {text.substring(0, 15) + "..."}
         </span>
       )}
       <span
-        className={`${className} hidden md:inline-flex items-center uppercase justify-center px-2 py-1 text-xs font-bold leading-none text-white  rounded`}
+        className={`${className} hidden md:inline-flex items-center uppercase justify-center px-2 py-1 text-xs font-bold leading-none   rounded`}
       >
         {text}
       </span>
