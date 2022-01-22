3d1e611561696074cd5f4025db3de422961c75e5
Philippe Naegeli <phn@anyweb.ch>
2022-01-22 15:03:31 -0500

    feat: add sign on transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.scss";
 import {
   ClerkProvider,
-  RedirectToSignIn,
   SignedIn,
   SignedOut,
 } from "@clerk/nextjs";
@@ -10,6 +9,7 @@ import { AppProps } from "next/dist/shared/lib/router/router";
 import Link from "next/link";
 import Layout from "../components/ui-layout/Layout";
 import { DefaultSeo, NextSeo } from "next-seo";
+import { Transition } from "@headlessui/react";
 
 const publicPages: string[] = [
   "/sign-in/[[...index]]",
@@ -25,19 +25,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider>
       <DefaultSeo
-      title="fintrack.io | Track you finances easily"
+        title="fintrack.io | Track you finances easily"
         openGraph={{
           type: "website",
           locale: "en_IE",
           url: "https://www.fintrackr.io/",
           site_name: "fintrackr",
-          description: 'Track your expenses easily',
+          description: "Track your expenses easily",
         }}
         twitter={{
           cardType: "summary_large_image",
         }}
       />
-          <NextSeo nofollow={true} />
+      <NextSeo nofollow={true} />
 
       {isPublicPage ? (
         <Component {...pageProps} />
@@ -49,18 +49,29 @@ function MyApp({ Component, pageProps }: AppProps) {
             </Layout>
           </SignedIn>
           <SignedOut>
-            <div className="signed-out-bg h-screen flex justify-center items-center text-center">
-              <main className="border-2 border-indigo-500 rounded-lg p-12 shadow-lg">
-                <h1 className="super-title"> fintrackr</h1>
-                <p>
-                  Please{" "}
-                  <Link href="/sign-in">
-                    <a className=" font-bold text-indigo-700">sign in</a>
-                  </Link>{" "}
-                  to access this page.
-                </p>
-              </main>
-            </div>
+            <Transition
+              show={true}
+              appear={true}
+              enter="transition-opacity duration-200 ease-in"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity duration-200 ease-in"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="signed-out-bg h-screen flex justify-center items-center text-center">
+                <main className="border-2 border-indigo-500 rounded-lg p-12 shadow-lg">
+                  <h1 className="super-title"> fintrackr</h1>
+                  <p>
+                    Please{" "}
+                    <Link href="/sign-in">
+                      <a className=" font-bold text-indigo-700">sign in</a>
+                    </Link>{" "}
+                    to access this page.
+                  </p>
+                </main>
+              </div>
+            </Transition>
           </SignedOut>
         </>
       )}
