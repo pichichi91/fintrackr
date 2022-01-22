59190aa7409b0505730b0ccba6ed99a4df38b57a
Philippe Naegeli <phn@anyweb.ch>
2022-01-22 00:25:30 -0500

    fix: cleanup animation

--- components/ui-layout/Layout.tsx
+++ components/ui-layout/Layout.tsx
@@ -9,21 +9,14 @@ type LayoutProps = {
 
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  
-  const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-
-    setLoaded(true);
-
-
-  }, [])
 
   return(
   <div className=" dur  mt-8 p-2 sm:p-4 flex justify-center w-full">
     <div className="mx-2 w-full sm:mx-12 lg:w-2/3  overflow-hidden  ">
     <Transition
-    show={loaded}
+    show={true}
+    appear={true}
     enter="transition-opacity duration-500 ease-in"
     enterFrom="opacity-0"
     enterTo="opacity-100"
