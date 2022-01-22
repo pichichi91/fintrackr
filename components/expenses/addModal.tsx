434f49a243ef7dea93389d672c70693a1a3d55e2
Philippe Naegeli <phn@anyweb.ch>
2022-01-22 01:01:49 -0500

    feat: update modal

--- components/expenses/addModal.tsx
+++ components/expenses/addModal.tsx
@@ -10,8 +10,9 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault("America/New_York");
 
-import currencies from './../../utils/currencies'
+import currencies from "./../../utils/currencies";
 import UiLoading from "../ui-loading/UiLoading";
+import { Dialog } from "@headlessui/react";
 type ExpenseProps = {
   date?: Dayjs;
   amount: number;
@@ -31,13 +32,12 @@ type AddExpenseModalProps = {
   user: string;
 };
 
-
 const AddExpenseModal: React.FC<AddExpenseModalProps> = ({
   setOpen,
   categories,
   reload,
   user,
-  isOpen
+  isOpen,
 }) => {
   const [newData, setNewData] = useState<ExpenseProps>({
     date: dayjs().utc(),
@@ -50,7 +50,6 @@ const AddExpenseModal: React.FC<AddExpenseModalProps> = ({
   });
 
   const [isLoading, setIsLoading] = useState(false);
-
 
   const changeSplit = (e: ChangeEvent<HTMLSelectElement>) => {
     const splitBy = e.target.value as "days" | "months";
@@ -105,17 +104,6 @@ const AddExpenseModal: React.FC<AddExpenseModalProps> = ({
     if (!error) setOpen(false);
     reload();
     setIsLoading(false);
-
-  };
-
-  const queryExpenses = async () => {
-    const { data, error } = await getExpenses({userId: user});
-
-    if (!error) {
-      console.log({ data });
-
-      setOpen(false);
-    }
   };
 
   const changeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -130,147 +118,154 @@ const AddExpenseModal: React.FC<AddExpenseModalProps> = ({
     // references are now sync'd and can be accessed.
   }
 
-  return isLoading ? <UiLoading /> :(
-    <div className={`${isOpen ? 'animate' : 'hidden'}`}>
-      <div className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative top-28 md:top-0 w-auto md:my-6 mx-auto max-w-3xl">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            <div className="flex mx-2 items-start justify-between pl-2 p-5 border-b border-solid border-blueGray-200 rounded-t">
-              <h3 className="text-2xl font-semibold">Add new expense</h3>
+  return (
+    <Dialog
+      className="hidden fixed z-10 inset-0 overflow-y-auto"
+      open={isOpen}
+      onClose={() => setOpen(false)}
+    >
+      <Dialog.Overlay className="fixed inset-0 -z-10 bg-black opacity-20" />
+
+      <div className="bg-white m-16   p-8 rounded-xl">
+        <Dialog.Title className="text-2xl font-semibold">
+          Add a new expense
+        </Dialog.Title>
+        <Dialog.Description className="mb-4">
+          <div className="relative  py-6 flex-auto flex flex-col md:flex-row ">
+            <div className=" md:basis-1/2 grid grid-cols-1 gap-6">
+              <label className="flex flex-col">
+                <span>Date</span>
+                <input
+                  onChange={changeDate}
+                  value={newData?.date?.format("YYYY-MM-DD")}
+                  type="date"
+                  className="styled-input"
+                />
+              </label>
+
+              <label className="flex flex-col">
+                <span>Currency</span>
+                <select
+                  onChange={changeCurrency}
+                  value={newData?.currency}
+                  className="styled-input"
+                >
+                  {currencies?.map((currency) => (
+                    <option key={currency.value} value={currency.value}>
+                      {currency.label}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label className="flex flex-col">
+                <span>Total</span>
+                <input
+                  onChange={changeAmount}
+                  value={newData?.amount}
+                  type="number"
+                  className="styled-input"
+                />
+              </label>
+              <label className="flex flex-col">
+                <span>Category</span>
+                <select
+                  onChange={changeCategory}
+                  value={newData?.category}
+                  className=" styled-input"
+                >
+                  <option value={0}>Select a category</option>
+
+                  {categories?.map((category) => (
+                    <option
+                      className="capitalize "
+                      key={category.id}
+                      value={category.id}
+                    >
+                      {category.name}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label className="flex flex-col">
+                <span>Split By</span>
+                <select
+                  onChange={changeSplit}
+                  value={newData?.splitBy}
+                  className="styled-input capitalize"
+                >
+                  {["days", "months"].map((item) => (
+                    <option className="capitalize " key={item} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label className="flex flex-col">
+                <span>Split to X {newData.splitBy}</span>
+                <input
+                  onChange={changeDays}
+                  value={newData?.days}
+                  type="number"
+                  className="styled-input"
+                />
+              </label>
             </div>
-            <div className="relative p-6 flex-auto flex flex-col md:flex-row ">
-              <div className="md:mx-2 grid grid-cols-1 gap-6">
+            <div className="flex flex-col  md:mt-0 md:ml-8">
+              <div className="">
                 <label className="flex flex-col">
-                  <span>Date</span>
-                  <input
-                    onChange={changeDate}
-                    value={newData?.date?.format("YYYY-MM-DD")}
-                    type="date"
-                    className="styled-input"
-                  />
-                </label>
+                  <span>Notes</span>
 
-                <label className="flex flex-col">
-                  <span>Currency</span>
-                  <select
-                    onChange={changeCurrency}
-                    value={newData?.currency}
-                    className="styled-input"
-                  >
-                    {currencies?.map((currency) => (
-                      <option key={currency.value} value={currency.value}>
-                        {currency.label}
-                      </option>
-                    ))}
-                  </select>
-                </label>
-                <label className="flex flex-col">
-                  <span>Total</span>
-                  <input
-                    onChange={changeAmount}
-                    value={newData?.amount}
-                    type="number"
-                    className="styled-input"
+                  <textarea
+                    value={newData.notes}
+                    onChange={changeNotes}
+                    rows={5}
+                    cols={25}
+                    className=" h-20 styled-input flex-1 "
                   />
-                </label>
-                <label className="flex flex-col">
-                  <span>Category</span>
-                  <select
-                    onChange={changeCategory}
-                    value={newData?.category}
-                    className=" styled-input"
-                  >
-                    <option value={0}>Select a category</option>
-
-                    {categories?.map((category) => (
-                      <option
-                        className="capitalize "
-                        key={category.id}
-                        value={category.id}
-                      >
-                        {category.name}
-                      </option>
-                    ))}
-                  </select>
-                </label>
-                <label className="flex flex-col">
-                  <span>Split By</span>
-                  <select
-                    onChange={changeSplit}
-                    value={newData?.splitBy}
-                    className="styled-input capitalize"
-                  >
-                    {["days", "months"].map((item) => (
-                      <option className="capitalize " key={item} value={item}>
-                        {item}
-                      </option>
-                    ))}
-                  </select>
-                </label>
-                <label className="flex flex-col">
-                  <span>Split to X {newData.splitBy}</span>
-                  <input
-                    onChange={changeDays}
-                    value={newData?.days}
-                    type="number"
-                    className="styled-input"
-                  />
-                  
                 </label>
               </div>
-              <div className="flex flex-col  md:mt-0 md:ml-8">
-                <div className="">
-                  <label className="flex flex-col">
-                    <span>Notes</span>
+              <div className="mt-4">
+                <h4 className=" font-bold text-lg">Summary</h4>
+                <div>
+                  {Array.from(Array(newData.days).keys())?.map((day) => {
+                    const date = newData.date
+                      ?.add(day, newData.splitBy)
+                      .format("DD MMM YYYY");
+                    return (
+                      <div key={date} className="flex  text-gray-500 text-xs">
+                        <p className="mr-2 w-20 font-bold">{date}</p>
 
-                    <textarea
-                      value={newData.notes}
-                      onChange={changeNotes}
-                      rows={5}
-                      cols={25}
-                      className=" h-20 styled-input flex-1 "
-                    />
-                  </label>
-                </div>
-                <div className="mt-4">
-                      <h4 className=" font-bold text-lg">Summary</h4>
-                      <div>
-                        {Array.from(Array(newData.days).keys())?.map((day) => {
-                          const date = newData.date?.add(day, newData.splitBy).format('DD MMM YYYY')
-                          return <div key={date} className="flex  text-gray-500 text-xs">
-                          <p className="mr-2 w-20 font-bold" >{date}</p>
-
-                          <p className="text-gray-400">
-                            {(newData.amount / newData.days).toFixed(2)}{" "}
-                            {currencies.find(currency => currency.value === newData.currency)?.label}
-                          </p>
-                        </div>
-                          
-                          
-                          
-                        })}
-                        
+                        <p className="text-gray-400">
+                          {(newData.amount / newData.days).toFixed(2)}{" "}
+                          {
+                            currencies.find(
+                              (currency) => currency.value === newData.currency
+                            )?.label
+                          }
+                        </p>
                       </div>
+                    );
+                  })}
                 </div>
               </div>
-            </div>
-            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-              <button
-                className=" danger"
-                type="button"
-                onClick={() => setOpen(false)}
-              >
-                Close
-              </button>
-              <button className="primary" type="button" onClick={handleSubmit}>
-                Save Changes
-              </button>
             </div>
           </div>
-        </div>
+        </Dialog.Description>
+
+
+<div className="flex  justify-end">
+        <button
+                className=" danger"
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </button>
+              <button className="primary" onClick={handleSubmit}>
+                Save Changes
+              </button>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-    </div>
+      </div>
+    </Dialog>
   );
 };
 export default AddExpenseModal;
